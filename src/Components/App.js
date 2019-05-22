e511d44a48bfb312470641ce089eb71e56f54482
hoxi777 <jtho1015@gmail.com>
2019-05-22 19:18:34 +0900

    update css and avatar && text css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { gql} from "apollo-boost";
-import { ThemeProvider } from "styled-components";
+import { gql } from "apollo-boost";
+import styled, { ThemeProvider } from "styled-components";
 import { HashRouter as Router } from "react-router-dom";
-import GlobalStyles from "../Styles/GlobalStyles"
+import { useQuery } from "react-apollo-hooks";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Routes from "./Routes";
-import { useQuery } from "react-apollo-hooks";
-import Footer from "./Footer"
+import Footer from "./Footer";
 import Header from "./Header";
-
-
 
 const QUERY = gql`
   {
@@ -17,23 +17,32 @@ const QUERY = gql`
   }
 `;
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
+
 export default () => {
   const {
     data: { isLoggedIn }
   } = useQuery(QUERY);
 
-return (
-  <ThemeProvider theme={Theme}>
-  <>
-    <GlobalStyles />
-    <Router>
+  return (
+    <ThemeProvider theme={Theme}>
       <>
-        {isLoggedIn && <Header />}
-          <Routes isLoggedIn={isLoggedIn} />
-          <Footer />
+        <GlobalStyles />
+        <Router>
+          <>
+            {isLoggedIn && <Header />}
+            <Wrapper>
+              <Routes isLoggedIn={isLoggedIn} />
+              <Footer />
+            </Wrapper>
+          </>
+        </Router>
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
       </>
-    </Router>
-  </>
-</ThemeProvider>
+    </ThemeProvider>
   );
-}
+};
--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -2,17 +2,18 @@ const BOX_BORDER = "1px solid #e6e6e6";
 const BORDER_RADIUS = "4px";
 
 export default {
-    bgColor:"#FAFAFA",
-    blackColor:"#262626",
-    blueColor:"#3897f0",
-    redColor:"#ED4956",
-    darkGreyColor:"#999",
-    lightGreyColor:"#c7c7c7",
-    darkBuleColor:"#003569",
-    boxBorder:"1px solid #e6e6e6",
-    borderRadius:"4px",
-    whiteBox: `${BOX_BORDER};
-               ${BORDER_RADIUS};
-               background-color: white;
-              `
+  maxWidth: "935px",
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBlueColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `border:${BOX_BORDER};
+             border-radius:${BORDER_RADIUS};
+             background-color:white;
+            `
 };
